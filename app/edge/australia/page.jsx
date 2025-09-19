158f19f5a5bcfb7d82cf1c2d6f186d1177bc3900
aasedeodb21-netizen <aasedeodb21@gmail.com>
2025-09-19 14:22:48 -0700

    Update page.jsx

--- app/edge/australia/page.jsx
+++ app/edge/australia/page.jsx
@@ -1,14 +1,1 @@
-import EdgeFunctionExplainer from '../explainer';
 
-export const metadata = {
-    title: 'In Australia'
-};
-
-export default function Page() {
-    return (
-        <>
-            <h1 className="mb-8">You are in Australia!</h1>
-            <EdgeFunctionExplainer />
-        </>
-    );
-}
