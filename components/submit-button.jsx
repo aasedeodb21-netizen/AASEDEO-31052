48d4dce47e8e6cb678932445253bd3de29c297a1
aasedeodb21-netizen <aasedeodb21@gmail.com>
2025-09-19 14:30:07 -0700

    Update submit-button.jsx

--- components/submit-button.jsx
+++ components/submit-button.jsx
@@ -1,12 +1,1 @@
-'use client';
 
-import { useFormStatus } from 'react-dom';
-
-export function SubmitButton({ text = 'Submit' }) {
-    const { pending } = useFormStatus();
-    return (
-        <button className="btn" type="submit" disabled={pending}>
-            {text}
-        </button>
-    );
-}
