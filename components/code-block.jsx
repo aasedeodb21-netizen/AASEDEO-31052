2a8fa8347993962e53ebe37f1f0dda0cc7b149cb
aasedeodb21-netizen <aasedeodb21@gmail.com>
2025-09-19 14:27:32 -0700

    Update code-block.jsx

--- components/code-block.jsx
+++ components/code-block.jsx
@@ -1,9 +1,1 @@
-import { Code } from 'bright';
 
-export function CodeBlock({ code, lang, lineNumbers, title }) {
-    return (
-        <Code lang={lang} title={title} lineNumbers={lineNumbers} theme="poimandres">
-            {code}
-        </Code>
-    );
-}
