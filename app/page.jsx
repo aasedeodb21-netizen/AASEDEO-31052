69d44957e8cec117c79da418f9114fe84dc6d2e2
aasedeodb21-netizen <aasedeodb21@gmail.com>
2025-09-19 13:35:40 -0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -28,10 +28,10 @@ export default function Page() {
         <div className="flex flex-col gap-12 sm:gap-16">
             <section>
                 <ContextAlert className="mb-6" />
-                <h1 className="mb-4">Netlify Platform Starter - Next.js</h1>
-                <p className="mb-6 text-lg">Get started with Next.js and Netlify in seconds.</p>
-                <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="btn btn-lg sm:min-w-64">
-                    Read the Docs
+                <h1 className="mb-4">AASE-EMIS DASHBOARD</h1>
+                <p className="mb-6 text-lg">Get Your Authentic and Reliable Data Here.</p>
+                <Link href="https://sites.google.com/view/aasedeo-user-login/staff-login" className="btn btn-lg sm:min-w-64">
+                    Staff Login
                 </Link>
             </section>
             {!!ctx && (
