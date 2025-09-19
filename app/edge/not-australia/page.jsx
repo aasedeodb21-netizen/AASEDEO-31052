3c379d6f1cb3d314a0be0d762ee8c371b549bed2
aasedeodb21-netizen <aasedeodb21@gmail.com>
2025-09-19 14:23:14 -0700

    Update page.jsx

--- app/edge/not-australia/page.jsx
+++ app/edge/not-australia/page.jsx
@@ -1,14 +1,1 @@
-import EdgeFunctionExplainer from '../explainer';
 
-export const metadata = {
-    title: 'Not Australia'
-};
-
-export default function Page() {
-    return (
-        <>
-            <h1 className="mb-8">You&apos;re not in Australia!</h1>
-            <EdgeFunctionExplainer />
-        </>
-    );
-}
